16ad000f33bfd59b73e0713100e131703f9f4949
Danila <kallaurdanila13@gmail.com>
2025-06-29 19:05:40 +0300

    add separate file(WayToTeach)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
 import Header from './components/Header'
 import { ways } from './data'
-
-function WayToTeach(props){
-  return(
-        <li>
-          <p>
-            <strong>{props.title}</strong> {props.description}
-          </p>
-        </li>
-  )
-}
+import WayToTeach from './components/WayToTeach'
 
 export default function App() {
   return (
@@ -20,23 +11,11 @@ export default function App() {
       <h3>Наш подход к обучению</h3>
 
       <ul>
-        <WayToTeach 
-        title={ways[0].title}
-        description = {ways[0].description}
-          />
-        <WayToTeach 
-        title={ways[1].title}
-        description = {ways[1].description} 
-         />
-        <WayToTeach 
-        title={ways[2].title}
-        description = {ways[2].description}
-          />
-        <WayToTeach 
-        title={ways[3].title}
-        description = {ways[3].description}
-          />
-
+        <WayToTeach {...ways[0]}/>
+        <WayToTeach {...ways[1]}/>
+        <WayToTeach {...ways[2]}/>
+        <WayToTeach {...ways[3]}/>
+        
       </ul>
     </section>
     </main>
