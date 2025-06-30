e3e649f4347f31e3f8e3d72797f5c79fd028751a
Danila <kallaurdanila13@gmail.com>
2025-06-30 15:03:09 +0300

    studiy react format

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -1,12 +1,28 @@
+import React, { createElement } from "react"
+
+// export default function IntroSection(){
+//     return (
+//         <section>
+//             <h1 className="centered">Result University</h1>
+//             <h3 className="centered" style={{color: '#666'}}>
+//             Университет frontend-разработки,
+//             который насыщает IT-сферу 
+//             квалифицированными программистами
+//             </h3>
+//         </section>
+//     )
+// }
+
+const e = React.createElement
+
 export default function IntroSection(){
-    return (
-        <section>
-            <h1>Result University</h1>
-            <h3>
-            Университет frontend-разработки,
-            который насыщает IT-сферу 
-            квалифицированными программистами
-            </h3>
-        </section>
-    )
+    return  e(
+        'section', 
+        null,[
+        e('h1', {className:'centered'}, 'Result University'),
+        e('h3', {className:'centered', style: {color: '#666'}}, 
+        'Университет frontend-разработки, который насыщает IT-сферу квалифицированными программистами'
+      
+        )
+    ])
 }
