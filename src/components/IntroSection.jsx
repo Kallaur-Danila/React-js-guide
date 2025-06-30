8b4afc25b77bff74d2ff04111d9cda030e1545a2
Danila <kallaurdanila13@gmail.com>
2025-06-30 15:27:16 +0300

    david pipis

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -19,8 +19,8 @@ export default function IntroSection(){
     return  e(
         'section', 
         null,[
-        e('h1', {className:'centered'}, 'Result University'),
-        e('h3', {className:'centered', style: {color: '#666'}}, 
+        e('h1', {className:'centered', key: 1}, 'Result University'),
+        e('h3', {className:'centered', style: {color: '#666'}, key: 2}, 
         'Университет frontend-разработки, который насыщает IT-сферу квалифицированными программистами'
       
         )
