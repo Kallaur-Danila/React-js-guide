85987cbf81fff5d63fafb2dd2cb344a96bba38dd
Danila <kallaurdanila13@gmail.com>
2025-06-30 13:49:52 +0300

    add intro_section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import Header from './components/Header'
 import TeachingSection from './components/TeachingSection'
 import DifferencesSection from './components/DifferencesSection'
+import IntroSection from './components/IntroSection'
+
 
 export default function App() {
 
   return (
-    <div>
-      <Header />
+    <>
+    <Header />
     <main>
+    <IntroSection />
     <TeachingSection />
     <DifferencesSection />
     </main>
-    </div>
+    </>
   ) 
 }
 
---
+++ src/components/IntroSection.jsx
@@ -0,0 +1,12 @@
+export default function IntroSection(){
+    return (
+        <section>
+            <h1>Result University</h1>
+            <h3>
+            Университет frontend-разработки,
+            который насыщает IT-сферу 
+            квалифицированными программистами
+            </h3>
+        </section>
+    )
+}
