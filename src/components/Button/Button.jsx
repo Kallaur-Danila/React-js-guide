bbff331959ea204c6dfc92a42a9f68869a011821
Danila <kallaurdanila13@gmail.com>
2025-07-01 14:28:52 +0300

    learn styled-components

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,14 @@
-import './Button.css'
+import classes from'./Button.module.css'
 
 export default function Button({children, onClick, isActive}){
   
     return (
-    <button className={isActive ? 'button active' : 'button'} onClick={onClick}>
-            {children}
+    <button 
+    className={
+        isActive ? `${classes.button} : ${classes.active}` : classes.button}
+         onClick={onClick}
+         >
+        {children}
     </button>
-
     )
 }
