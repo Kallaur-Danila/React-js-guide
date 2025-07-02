41ca3208bf9ace88812b66f17b7da149448ac69f
Danila <kallaurdanila13@gmail.com>
2025-07-02 18:13:26 +0300

    start work with use_effect

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,16 @@
 import { createPortal } from 'react-dom'
+import { useRef } from 'react'
 import './Modal.css'
 
 export default function Modal({children, open}){
+    const dialog = useRef() 
+    
+    if(open){
+        dialog.current.showModal()
+    } else{
+        dialog.current.closes()
+    }
+
     return createPortal(
         <dialog open={open}>{children}</dialog>,
         document.getElementById('modal')
